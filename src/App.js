5327cec21a2284ac1a2b634aaaf94eeb002eb0f7
csengmany <sengmany.cathy@gmail.com>
2021-02-22 00:29:42 +0100

    correct a bug

--- src/App.js
+++ src/App.js
@@ -77,16 +77,16 @@ function App() {
         //axios request
         const fetchData = async () => {
             try {
-                const responseUser = await axios.get(
-                    `https://cathy-marvel-backend.herokuapp.com/user/favorites/${userId}`
-                );
+                // const responseUser = await axios.get(
+                //     `https://cathy-marvel-backend.herokuapp.com/user/favorites/${userId}`
+                // );
 
-                console.log(
-                    "data favorite page",
-                    responseUser.data.favorite_characters,
-                    responseUser.data.favorite_comics
-                );
-                setUserData(responseUser.data);
+                // console.log(
+                //     "data favorite page",
+                //     responseUser.data.favorite_characters,
+                //     responseUser.data.favorite_comics
+                // );
+                // setUserData(responseUser.data);
                 const response = await axios.get(
                     `https://cathy-marvel-backend.herokuapp.com/characters?name=${search}&page=${page}&limit=${limit}`
                 );
